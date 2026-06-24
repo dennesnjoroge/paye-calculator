c8f03fe55bb5ed90dfc314e766601f44a8c65d64
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-06-25 01:15:27 +0300

    feat: set views directory for EJS template engine

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 // static files, view engine
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // routes config
 app.use("/", appRoutes);
